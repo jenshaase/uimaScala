39dfd1a5b65fee362640a1aabf8730fde91f38d3
Jens Haase <je.haase@googlemail.com>
2011-10-07 19:03:04 +0200

    Added some convert tests

--- uima-core/src/test/scala/jenshaase/uimaScala/core/ConverterSpec.scala
+++ uima-core/src/test/scala/jenshaase/uimaScala/core/ConverterSpec.scala
@@ -5,6 +5,10 @@ package jenshaase.uimaScala.core
 
 import org.specs2._
 import matcher._
+import java.util.regex.Pattern
+import java.util.Locale
+import java.io.File
+import scala.util.matching.Regex
 
 class ConverterSpec extends Specification with DataTables {
   import CastFactory._
@@ -12,11 +16,20 @@ class ConverterSpec extends Specification with DataTables {
   // format: OFF
   def is =
     "The Formatter should"                    ^
-      "convert 'test'"                        ! to("test", "test")^
-      "convert 1"                             ! to(1, 1)^
-      "convert List(1, 2)"                    ! to(List(1, 2), Array(1, 2))^
-      "convert List('t', 'v')"                ! to(List("t", "v"), Array("t", "v"))
-      // "convert List(1, 2)"                    ! from(List(1, 2))^
+      "convert string"                        ! convert("test", "test")^
+      "convert int"                           ! convert(1, 1)^
+      "convert float"                         ! convert(1.2f, 1.2f)^
+      "convert boolean"                       ! convert(true, true)^
+      "convert locale"                        ! convert(new Locale("en"), "en")^
+      "convert pattern"                       ! convertPattern(Pattern.compile("[A-Z]*"), "[A-Z]*")^
+      "convert regex"                         ! convertRegex("[A-Z]*".r, "[A-Z]*")^
+      "convert file"                          ! convert(new File("/test/abc"), "/test/abc")^
+      "convert string list"                   ! convert(List("t", "v"), Array("t", "v"))^
+      "convert int list"                      ! convert(List(1, 2), Array(1, 2))^
+      "convert float list"                    ! convert(List(1.2f, 2.3f), Array(1.2f, 2.3f))^
+      "convert boolean list"                  ! convert(List(true, false, true), Array(true, false, true))^
+      "convert locale list"                   ! convert(List(new Locale("de"), new Locale("en")), Array("de", "en"))^
+      "convert file list"                     ! convert(List(new File("/test/a"), new File("/test/b")), Array("/test/a", "/test/b"))
     end
 
 
@@ -33,9 +46,39 @@ class ConverterSpec extends Specification with DataTables {
     fromAny(toAny(in).right.get, m.erasure).right.get.asInstanceOf[T] must_== in
   }*/
   
-  def to[T, R](in: T, out: R)(implicit m: Manifest[T]) = {
-    toUima(in) must beRight
-    toUima(in).right.get must beSome
-    toUima(in).right.get.get must_== out
+  def convert[T, R](in: T, out: R)(implicit m: Manifest[T], r: Manifest[R]) = {
+    val to = toUima(in)
+    to must beRight
+    to.right.get must beSome
+    to.right.get.get must_== out
+
+    val from = fromUima[T](out)
+    from must beRight
+    from.right.get must beSome
+    from.right.get.get must_== in
+  }
+
+  def convertPattern(in: Pattern, out: String) = {
+    val to = toUima(in)
+    to must beRight
+    to.right.get must beSome
+    to.right.get.get must_== out
+
+    val from = fromUima[Pattern](out)
+    from must beRight
+    from.right.get must beSome
+    from.right.get.get.pattern must_== in.pattern
+  }
+
+  def convertRegex(in: Regex, out: String) = {
+    val to = toUima(in)
+    to must beRight
+    to.right.get must beSome
+    to.right.get.get must_== out
+
+    val from = fromUima[Regex](out)
+    from must beRight
+    from.right.get must beSome
+    from.right.get.get.pattern.pattern must_== in.pattern.pattern
   }
 }
